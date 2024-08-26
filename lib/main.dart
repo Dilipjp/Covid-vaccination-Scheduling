75f3344eeab70f8db154ed95b83530e1587aa0d5
Dilipjp <jpdilipk@gmail.com>
2024-08-27 00:27:06 +0530

    Main changed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
   await Firebase.initializeApp();
 
   runApp(const MyApp());
-  addCentersToFirestore();
+  // addCentersToFirestore();
 }
 
 class MyApp extends StatelessWidget {
