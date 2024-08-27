ce930f40d5b261cec590a9fdfe94856966a02ab1
Dilipjp <jpdilipk@gmail.com>
2024-08-27 11:42:39 +0530

    Schedule changed

--- lib/model/Schedule.dart
+++ lib/model/Schedule.dart
@@ -1,28 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Schedule {
   final String id;
-  final DateTime date;
-  final String vaccineName;
-  final String center; // Changed from VaccinationCenter to String
-  final String additionalInfo;
   final String userId;
+  final String vaccineName;
+  final DateTime date;
+  final String additionalInfo;
+  final String center;
 
   Schedule({
     required this.id,
-    required this.date,
-    required this.vaccineName,
-    required this.center, // Handle as string
-    required this.additionalInfo,
     required this.userId,
+    required this.vaccineName,
+    required this.date,
+    required this.additionalInfo,
+    required this.center,
   });
 
   factory Schedule.fromMap(Map<String, dynamic> data) {
     return Schedule(
       id: data['id'] ?? '',
-      date: DateTime.parse(data['date']), // Ensure the date is parsed correctly
-      vaccineName: data['vaccineName'] ?? '',
-      center: data['center'] ?? '', // Just store the string as it is
-      additionalInfo: data['additionalInfo'] ?? '',
       userId: data['userId'] ?? '',
+      vaccineName: data['vaccineName'] ?? '',
+      date: (data['date'] as Timestamp).toDate(), // Convert Timestamp to DateTime
+      additionalInfo: data['additionalInfo'] ?? '',
+      center: data['center'] ?? '',
     );
   }
 }
